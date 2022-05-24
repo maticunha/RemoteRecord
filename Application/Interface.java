96de53aa41b28fb7bd8bd440ce0cb4a964b470ac
levinej1 <levinej1@wit.edu>
2022-05-24 14:30:09 -0400

    simple sound recording

--- Application/Interface.java
+++ Application/Interface.java
@@ -1,4 +1,6 @@
+package Application;
 
+	
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -6,11 +8,10 @@ import javafx.scene.layout.Pane;
 import javafx.fxml.FXMLLoader;
 
 
-public class Interface extends Application {
-	@Override
+public class Interface extends Application{
 	public void start(Stage primaryStage) {
 		try {
-			Pane root = (Pane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Pane root = (Pane)FXMLLoader.load(getClass().getResource("Sample1.fxml"));
 			Scene scene = new Scene(root,600,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
@@ -20,18 +21,8 @@ public class Interface extends Application {
 		}
 	}
 	
-	public void settings(Stage primaryStage) {
-		try {
-			Pane root = (Pane)FXMLLoader.load(getClass().getResource("Settings.fxml"));
-			Scene scene = new Scene(root,600,300);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
 	public static void main(String[] args) {
 		launch(args);
 	}
+
 }
