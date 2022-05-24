8d2ee7e4fe3724fce469eb77e92ac7e460d0a104
joshlevine55 <joshlevine55@gmail.com>
2022-05-24 11:50:10 -0400

    Testing the waters here

---
+++ Interface.java
@@ -0,0 +1,37 @@
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Interface extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Pane root = (Pane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root,600,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void settings(Stage primaryStage) {
+		try {
+			Pane root = (Pane)FXMLLoader.load(getClass().getResource("Settings.fxml"));
+			Scene scene = new Scene(root,600,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
