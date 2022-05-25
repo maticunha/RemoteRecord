328ee612854af93bc289e1ddfe96870579f58360
maticunha <cunham1@wit.edu>
2022-05-25 12:10:48 -0400

    Starting Record class, pushing to create Mati and Josh branches

---
+++ Application/Record.java
@@ -0,0 +1,18 @@
+package Application;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.TargetDataLine;
+
+public class Record {
+	
+	private AudioFormat audioFormat;
+	private DataLine.Info dataInfo;
+	private TargetDataLine dataLine; 
+	private Thread audioInput;
+	
+	
+	
+	
+
+}
